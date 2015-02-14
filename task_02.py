dca01e5d355a9638d8c094f76d43a405919469c6
Emadul Islam <phemadul@gmail.com>
2015-02-14 06:06:48 +0000

    added task_02.py file

---
+++ task_02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Python's order of operations respects parentheses.
+
+ Create a mathematical statement in a single-line.
+"""
+WEEKS = (19%10+100+2**8)//7
+print WEEKS
+
+
+
