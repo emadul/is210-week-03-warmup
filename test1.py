9f0d3c8844a16b9eff23002ae34549504041add8
Emadul Islam <phemadul@gmail.com>
2015-02-14 16:44:39 +0000

    added split string

---
+++ test1.py
@@ -0,0 +1,12 @@
+Python 2.7.6 (default, Mar 22 2014, 22:59:38) 
+[GCC 4.8.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+line = 'Go:  Tear some strings apart!'
+seq = line.split()
+seq
+line.split(':')
+line.split('ar')
+lines = 'This includes\\nsome new\\nlines.'
+lines.split()
+ 
+print lines 
